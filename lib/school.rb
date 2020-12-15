f23125ca5200e9e607f064c64f3a26801444fac1
Alex <alexfrancisaaron@gmail.com>
2020-12-15 16:02:36 -0600

    commit tree e096dab76978825a10deb0bb93c107cb0c870d50

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
 # code here!
+class School 
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def name
+        @name
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student, grade)
+        if @roster.include?(grade)
+            @roster[grade] << student
+        else
+            @roster[grade] = []
+            @roster[grade] << student
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        new_hash = {}
+        @roster.each do |grade, students|
+            sorted_grade = @roster[grade].sort
+            new_hash[grade] = sorted_grade
+        end
+        new_hash
+    end
+end
